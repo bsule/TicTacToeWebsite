a2262f940ef2f05471043c5b6aef270344c13d46
bsule <bilal.suleiman@gmail.com>
2023-05-04 08:31:34 -0700

    allows user to choose which color to play with

--- TicTacToe/myscript.js
+++ TicTacToe/myscript.js
@@ -1,7 +1,27 @@
 
 var userInput=localStorage.getItem("range");
+var color;
+
+if(userInput!=1 && userInput!=2 && userInput!=3){
+    userInput=1;
+}
+
+if(userInput==1){
+    color='red';
+    document.getElementById("topText").innerHTML="You are Red, the Computer is Black";
+}
+else if(userInput==2){
+    color='purple';
+    document.getElementById("topText").innerHTML="You are Purple, the Computer is Black";
+}
+else{
+    color='white';
+    document.getElementById("topText").innerHTML="You are White, the Computer is Black";
+}
+
+var board = [['','',''],['','',''],['','','']];
 
 $("td").click(function(){
-    $(this).find('button').css('background-color','red');
+    $(this).find('button').css('background-color',color);
     console.log(userInput);
 })
