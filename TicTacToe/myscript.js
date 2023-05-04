c94b3b21bbc4908b98f50ca8332338b1875f4758
bsule <bilal.suleiman@gmail.com>
2023-05-04 12:44:21 -0700

    added part of checkWin()

--- TicTacToe/myscript.js
+++ TicTacToe/myscript.js
@@ -41,7 +41,7 @@ else{
 function computerStart(){
     return Math.floor(Math.random()*9)+1;
 }
-
+// if computer has first move
 if(startValue==1){
     var computerSquare = computerStart();
     if(computerSquare==1){
@@ -90,33 +90,162 @@ function userTurn(){
     $("td").click(function(){
         if(clickedId=="one" && board[0][0]==''){
             $(this).find('button').css("background-color",color);
+            board[0][0]='0';
+            checkWin();
+            computerMove();
         }
         else if(clickedId=="two" && board[0][1]==''){
             $(this).find('button').css("background-color",color);
+            board[0][1]='0';
+            checkWin();
+            computerMove();
         }
         else if(clickedId=="three" && board[0][2]==''){
             $(this).find('button').css("background-color",color);
+            board[0][2]='0';
+            checkWin();
+            computerMove();
         }
         else if(clickedId=="four" && board[1][0]==''){
             $(this).find('button').css("background-color",color);
+            board[1][0]='0';
+            checkWin();
+            computerMove();
         }
         else if(clickedId=="five" && board[1][1]==''){
             $(this).find('button').css("background-color",color);
+            board[1][1]='0';
+            checkWin();
+            computerMove();
         }
         else if(clickedId=="six" && board[1][2]==''){
             $(this).find('button').css("background-color",color);
+            board[1][2]='0';
+            checkWin();
+            computerMove();
         }
         else if(clickedId=="seven" && board[2][0]==''){
             $(this).find('button').css("background-color",color);
+            board[2][0]='0';
+            checkWin();
+            computerMove();
         }
         else if(clickedId=="eight" && board[2][1]==''){
             $(this).find('button').css("background-color",color);
+            board[2][1]='0';
+            checkWin();
+            computerMove();
         }
-        else if(clickedId=="two" && board[2][2]==''){
+        else if(clickedId=="nine" && board[2][2]==''){
             $(this).find('button').css("background-color",color);
+            board[2][2]='0';
+            checkWin();
+            computerMove();
         }
     })
     
+}
+
+function computerMove(){
+    
+   
+    $("#five").find('button').css("background-color", "black");
+    console.log("hello")
+
+    checkWin();
+}
+
+function checkWin(){
+    var computerWinning = false;
+    var userWinning = false;
+    var won=2;
+    for(var i=0;i<3;i++){ // horizontal win
+        for(var j=0;j<3;j++){
+            if(j==0 && board[i][j]=='0'){
+                userWinning=true;
+            }
+            else if(j==0 && board[i][j]=='1'){
+                computerWinning = true;
+            }
+            else if((j==1 || j==2) && board[i][j]=='0'){
+                if(userWinning==true && j==1){
+                    continue;
+                }
+                else if(userWinning==true && j==2){
+                    won=0;
+                    console.log("won");
+                }
+                else if(userwinning==false){
+                    userWinning=false;
+                    computerWinning = false;
+                    break;
+                }
+            }
+            else if((j==1 || j==2) && board[i][j]=='1'){
+                if(computerWinning==true && j==1){
+                    continue;
+                }
+                else if(computerWinning==true && j==2){
+                    won=1;
+                }
+                else if(computerWinning==false){
+                    userWinning=false;
+                    computerWinning = false;
+                    break;
+                }
+            }
+            else{
+                userWinning=false;
+                computerWinning = false;
+                break;
+            }
+        }
+    }
+
+    for(var i=0;i<3;i++){ // vertical win
+        for(var j=0;j<3;j++){
+            if(j==0 && board[j][i]=='0'){
+                userWinning=true;
+            }
+            else if(j==0 && board[j][i]=='1'){
+                computerWinning = true;
+            }
+            else if((j==1 || j==2) && board[j][i]=='0'){
+                if(userWinning==true && j==1){
+                    continue;
+                }
+                else if(userWinning==true && j==2){
+                    won=0;
+                    console.log("won");
+                }
+                else if(userwinning==false){
+                    userWinning=false;
+                    computerWinning = false;
+                    break;
+                }
+            }
+            else if((j==1 || j==2) && board[j][i]=='1'){
+                if(computerWinning==true && j==1){
+                    continue;
+                }
+                else if(computerWinning==true && j==2){
+                    won=1;
+                }
+                else if(computerWinning==false){
+                    userWinning=false;
+                    computerWinning = false;
+                    break;
+                }
+            }
+            else{
+                userWinning=false;
+                computerWinning = false;
+                break;
+            }
+        }
+    }
+
+    return won;
 }
 
 userTurn();
